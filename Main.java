2d1f7f897abde574270c14c79099189aac09fb34
MdTazmulIslam206 <142989526+MdTazmulIslam206@users.noreply.github.com>
2025-12-02 20:25:57 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //Simple Printing Program
+        //Name
+        System.out.println("Md:Tazmul Islam");
+        //Course name
+        System.out.println("Backend Web Development with Java & SpringBoot.");
+        //My current goal
+        System.out.println("My goal is to master Web Development.");
+        //Date
+        System.out.println("Dec 1 ,2025");
+        //Message of frends
+        System.out.println("Hello, fellow developers!");
+
+        //Module 1 Learning Summary
+        System.out.println("Java Setup & Basics");
+        System.out.println("I learned a lot in this module. like : History & Uses of Java | Java Platform (JVM, JRE, JDK) | Installing Java & Setting JAVA_HOME | Choosing & Configuring an IDE | Writing & Running HelloWorld.java");
+        System.out.println("I have created several Java projects.");
+    }
+}
